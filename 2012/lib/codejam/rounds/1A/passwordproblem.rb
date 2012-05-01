d86889a9d77c941f1a6d698cf91b58b5c3c05b4e
Esther Schmitz <eddaflux@gmail.com>
2012-05-01 19:20:40 +0200

    1A - password problem - correct and fast

--- 2012/lib/codejam/rounds/1A/passwordproblem.rb
+++ 2012/lib/codejam/rounds/1A/passwordproblem.rb
@@ -16,11 +16,9 @@ class PasswordProblem < Solver
 
       @test_cases << test_case
     end
-    # puts "TEST CASES: #{@test_cases.inspect}"
   end
   
   def solve
-    
    @test_case_count.times do |case_i|
       test_case = @test_cases[case_i]
       chars_typed = test_case[0][0].to_i
@@ -34,22 +32,15 @@ class PasswordProblem < Solver
       stroke_count_enter_immediately = total_chars + 2
       
       # calculate expected keystrokes
-      p_all_correct = p_entry_correct(probabilities)
-      # puts "Probabilities: #{probabilities.inspect}"
-      # puts "p_all_correct = #{p_all_correct}"
+      p_all_correct = probabilities.inject(:*)
       keep_going_expected = p_all_correct * stroke_count_keep_going + (1 - p_all_correct) * (stroke_count_keep_going + stroke_count_repeat)
       enter_immediately_expected = stroke_count_enter_immediately
       
-      expectations = [keep_going_expected, enter_immediately_expected]
-      if (chars_typed > 0)
-        (1..chars_typed).each do |i|
-          expectations << back_i_expected(probabilities, p_all_correct, total_chars, remainder, i)
-        end
-      end
+      @expectations = [keep_going_expected, enter_immediately_expected]
+      calculate_back_i_expected(probabilities, total_chars, chars_typed, remainder)
       
-      # puts "Expectations: #{expectations.inspect}"
-      
-      print("%.6f" % expectations.min.round(6), case_i + 1)
+      # make sure to display result with a precision of 6 digits (pad with 0s if necessary)
+      print("%.6f" % @expectations.min.round(6), case_i + 1)
     end
     
   end
@@ -58,32 +49,16 @@ class PasswordProblem < Solver
     i*2 + remainder + 1
   end
   
-  def back_i_expected(probabilities, p_all_correct, total, remainder, i)
-    stroke_back_i = stroke_count_back_i(remainder, i)
-    p_error_last_i = p_error_only_in_last_i_chars(probabilities, i)
-    p_repeat_needed = 1 - p_all_correct - p_error_last_i
-    
-    p_all_correct * stroke_back_i + p_error_last_i * stroke_back_i + p_repeat_needed * (stroke_back_i + total + 1)
-  end
-  
-  def p_entry_correct(probabilities)
-    probabilities.inject(:*)
-  end
-  
-  def p_error_only_in_last_i_chars(probabilities, i)
-    p_no_error_before_last_i_chars = probabilities[0..probabilities.length-i-1].inject(:*)
-    p_at_least_1_error = p_at_least_1_error_in_last_i_chars(probabilities, i)
-    
-    # puts "Back #{i} --- No Error before last #{i}: #{p_no_error_before_last_i_chars}"
-    # puts "Back #{i} --- Error in last #{i}: #{p_at_least_1_error}"
-    
-    p_no_error_before_last_i_chars * p_at_least_1_error
-  end
-  
-  def p_at_least_1_error_in_last_i_chars(probabilities, i)
-    p_no_error_in_last_i = probabilities[probabilities.length - i..probabilities.length - 1].inject(:*)
-    # puts "No error in last i. i = #{i} -- p = #{p_no_error_in_last_i}"
-    (1 - p_no_error_in_last_i)
+  def calculate_back_i_expected(probabilities, total, chars_typed, remainder)
+    prob_success = 1
+    # reuse previous probability calculation. Start with prob of 1 for the case where we backspace all typed chars
+    # the probability that we get the correct password on first try will get smaller and smaller as we progress to fewer backspaces
+    chars_typed.times do |i|
+      stroke_back_i = stroke_count_back_i(remainder, chars_typed - i)
+      @expectations << prob_success * stroke_back_i + (1 - prob_success) * (stroke_back_i + total + 1)
+      # add probability for next char to have been typed correctly
+      prob_success *= probabilities[i]
+    end
   end
   
   def print(result, i)
